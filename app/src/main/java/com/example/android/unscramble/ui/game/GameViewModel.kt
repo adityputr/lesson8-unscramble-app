b3dacee796001f214b51734447df7e33f2ebeacc
salmawahyun <salmawahyun@gmail.com>
2023-09-27 15:36:21 +0700

    menambahkan fungsi isUserWordCorrect yang menampilkan Boolean dan mengambil String untuk memvalidasikan kata dari pemain dan tingkatkan skor jika tebakannya benar

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -46,6 +46,13 @@ class GameViewModel : ViewModel() {
     private fun increaseScore() {
         _score += SCORE_INCREASE
     }
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
     fun nextWord(): Boolean {
         return if (currentWordCount < MAX_NO_OF_WORDS) {
             getNextWord()
